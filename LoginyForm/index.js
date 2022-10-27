0da555807265cdad4979462d905da5c0b0287c72
Dominguezd01 <migueldominguezroman01@gmail.com>
2022-10-27 19:04:56 +0200

    27/10/22

--- LoginyForm/index.js
+++ LoginyForm/index.js
@@ -1,24 +1,33 @@
 window.onload = function (){
-    document.querySelector("form")
-    .addEventListener('submit', e => {
-      e.preventDefault()
-      const data = Object.fromEntries(
-        new FormData(e.target)
-
-      )
-      console.log(data)
-      recogerValores(JSON.stringify(data))
-    })
+   cargarForm()
 }
 
+const cargarForm = () =>{
+  document.querySelector("form")
+  .addEventListener('submit', e => {
+    e.preventDefault()
+    const data = Object.fromEntries(
+      new FormData(e.target)
 
+    )
+    console.log(data)
+    recogerValores(JSON.stringify(data))
+  })
+}
 
 const recogerValores = (data) =>{
     let obj = JSON.parse(data)
-     console.log(obj)
-    return JSON.stringify(obj.userName)
+    console.log(obj)
+    localStorage.setItem("nomUsuario", obj.userName)
+    let form = document.getElementById("form")
+    form.style.display = "none"
+}
+const crearH1 = (usuario) =>{
+   
+    let h1 = document.createElement("h1")
 
+    h1.innerHTML += "Hola "+ usuario + " introduce tus datos"
+
+    document.body.append(h1)
 }
 
-let nomUsuario = recogerValores();
-console.log(nomUsuario)
